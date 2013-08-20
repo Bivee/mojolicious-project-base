d35287e33cc1c0129a518b88a9f736a506af4635
Daniel Vinciguerra <dan.vinciguerra@gmail.com>
2013-08-20 17:05:33 -0300

    adicionando classe de rotas customizadas

--- lib/MyAPP.pm
+++ lib/MyAPP.pm
@@ -1,6 +1,8 @@
 package MyAPP;
 use Mojo::Base 'Mojolicious';
 
+use MyAPP::Routes;
+
 sub startup {
     my $self = shift;
 
@@ -10,6 +12,9 @@ sub startup {
 
     # Normal route to controller
     $r->any('/')->to('home#index');
+
+    # configure custom routes
+    MyAPP::Routes->load($r);
 
     $r->any('/:controller/:action/:id')
       ->to( controller => 'home', action => 'index', id => 0 );
