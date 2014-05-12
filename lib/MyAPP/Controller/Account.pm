1fde621763f6271776e26aaafb71b2dae7a46044
Daniel Vinciguerra <dan.vinciguerra@gmail.com>
2014-05-12 00:15:32 -0300

    add Account controller

---
+++ lib/MyAPP/Controller/Account.pm
@@ -0,0 +1,16 @@
+package MyAPP::Controller::Account;
+use Mojo::Base 'MyAPP::Controller::Base';
+
+sub login {
+    my $self = shift;
+}
+
+sub logout {}
+
+sub register {}
+
+sub forgot {}
+
+sub change {}
+
+1;
